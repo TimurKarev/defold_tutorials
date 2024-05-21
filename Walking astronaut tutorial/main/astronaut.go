779307820fcf652693e523af8d849782c3d753c1
karevbob <karevbob@bk.ru>
2024-05-21 22:30:32 +0700

    he can walk :)
    HE CAN WALK OK DONT WORRY

--- Walking astronaut tutorial/main/astronaut.go
+++ Walking astronaut tutorial/main/astronaut.go
@@ -16,14 +16,17 @@ components {
 embedded_components {
   id: "sprite"
   type: "sprite"
-  data: "tile_set: \"/main/astronaut.atlas\"\n"
-  "default_animation: \"idle\"\n"
+  data: "default_animation: \"idle\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/astronaut.atlas\"\n"
+  "}\n"
   ""
   position {
-    x: 0.0
-    y: 0.0
+    x: -1.0
+    y: -6.0
     z: 0.0
   }
   rotation {
