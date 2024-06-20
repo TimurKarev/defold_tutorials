b5f95e5195e8ca09e6ef2f7a153c798ef2c62cd2
karevbob <karevbob@bk.ru>
2024-06-20 20:23:20 +0700

    alot of work

---
+++ StateMachine/prefabs/player move/p1 p2.go
@@ -0,0 +1,38 @@
+components {
+  id: "p1 p2"
+  component: "/prefabs/player move/p1 p2.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"p1\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/prefabs/player move/p1 p2.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
